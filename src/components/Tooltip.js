5fd8b324dcb0cdc082d2aaaa33801bfada1b32d4
anitha-anii <anithachowdary355@gmail.com>
2023-07-12 12:46:45 +0530

    'changes'

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Tooltip = ({ text, children }) => {
   const [isTooltipVisible, setTooltipVisible] = useState(false);
@@ -21,24 +21,22 @@ const Tooltip = ({ text, children }) => {
   };
 
   return (
-    <div>
-      <h2 onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <div className="tooltiptext">
+      <h2 className="tooltip" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{isTooltipVisible && <div>{text}</div>}
         Hover over me
       </h2>
       <hr />
-      <h6
+
+      <p className="tooltip"
         onMouseEnter={handleChildrenMouseEnter}
         onMouseLeave={handleChildrenMouseLeave}
-      >
-        Hover over me to see another tooltip
-      </h6>
+      >Hover over me to see another tooltip
+       {isChildrenTooltipVisible && (<div>{children}</div>)}
+      </p>
       <hr />
-      {isTooltipVisible && <div className='tooltiptext'>{text}</div>}
-      {isChildrenTooltipVisible && (
-        <div className='tooltiptext'>{children}</div>
-      )}
-    </div>
-  );
+              </div>
+          
+            );
 };
 
 export default Tooltip;

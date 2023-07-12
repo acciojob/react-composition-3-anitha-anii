1f1cf0a6216fc91e11bc13de7e1b4dbecdbefe93
anitha-anii <anithachowdary355@gmail.com>
2023-07-12 13:09:16 +0530

    'changes'

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -21,22 +21,20 @@ const Tooltip = ({ text, children }) => {
   };
 
   return (
-    <div className="tooltiptext">
-      <h2 className="tooltip" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{isTooltipVisible && <div>{text}</div>}
+    <div>
+      <h2 className={`tooltip ${isTooltipVisible ? "active" : ""}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         Hover over me
+        {isTooltipVisible && <div className="tooltiptext">{text}</div>}
       </h2>
       <hr />
 
-      <p className="tooltip"
-        onMouseEnter={handleChildrenMouseEnter}
-        onMouseLeave={handleChildrenMouseLeave}
-      >Hover over me to see another tooltip
-       {isChildrenTooltipVisible && (<div>{children}</div>)}
+      <p className={`tooltip ${isChildrenTooltipVisible ? "active" : ""}`} onMouseEnter={handleChildrenMouseEnter} onMouseLeave={handleChildrenMouseLeave}>
+        Hover over me to see another tooltip
+        {isChildrenTooltipVisible && <div className="tooltiptext">{children}</div>}
       </p>
       <hr />
-              </div>
-          
-            );
+    </div>
+  );
 };
 
 export default Tooltip;

0ca91948d8ea2c2531946145773244f80da8f455
anitha-anii <anithachowdary355@gmail.com>
2023-07-14 15:29:56 +0530

    'chnages

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,83 @@
-import React from 'react';
-import Tooltip from './Tooltip';
+// import React from 'react';
+// import {Tooltip,Child} from './Tooltip';
+// import '../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Tooltip text="This is a tooltip">
+//        </Tooltip>
+
+//        <Child children='This is another tooltip'>
+//        </Child>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import React, { useState } from 'react';
 import '../styles/App.css';
+
+const Tooltip = ({ text }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return (
+    <h2
+      className="tooltip"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      Hover over me
+      {showTooltip && <div className="tooltiptext">{text}</div>}
+    </h2>
+  );
+};
+
+const Child = ({ children }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return (
+    <p
+      className="tooltip"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      Hover over me to see another
+      {showTooltip && <div className="tooltip">{children}</div>}
+    </p>
+  );
+};
 
 const App = () => {
   return (
     <div>
-      <Tooltip text="This is a tooltip">
-        <h2 className="tooltip">Hover over me</h2>
-      </Tooltip>
-
-      <Tooltip text="This is another tooltip">
-        <p className="tooltip">Hover over me to see another tooltip</p>
-      </Tooltip>
+      <Tooltip text="This is a tooltip" />
+      <Child>This is another tooltip</Child>
     </div>
   );
 };
 
 export default App;
+export { Tooltip, Child };
+
 
 
 

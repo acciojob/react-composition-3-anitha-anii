443e0d51c1939f2a222cd5bb744cdec11ac5cd04
anitha-anii <anithachowdary355@gmail.com>
2023-07-25 12:44:38 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tooltip from './Tooltip';
 import '../styles/App.css';
-import Tooltiptext from './TooltipText';
+
 
 function App() {
   return (
@@ -9,11 +9,7 @@ function App() {
       <Tooltip text="This is a tooltip">
         <span className="tooltip">Hover over me</span>
       </Tooltip>
-    <br />
-      <Tooltiptext text="This is another tooltip">
-        <span className="tooltip">Hover over me to another tooltip</span>
-       </Tooltiptext>
-    </div>
+        </div>
   );
 }
 
